da5460c7b7a46b7743554e9f24de5e4766bbd5ab
Yens Broothaers <yens.broothaers@student.arteveldehs.be>
2018-11-07 19:45:23 +0100

    Add authorization with headers

--- src/app/authentication/login/login.component.ts
+++ src/app/authentication/login/login.component.ts
@@ -1,15 +1,34 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit } from "@angular/core";
+import { UserService } from "src/app/services/user.service";
+import { Router } from "@angular/router";
 
 @Component({
-  selector: 'app-login',
-  templateUrl: './login.component.html',
-  styleUrls: ['./login.component.scss']
+  selector: "app-login",
+  templateUrl: "./login.component.html",
+  styleUrls: ["./login.component.scss"]
 })
 export class LoginComponent implements OnInit {
+  username;
+  password;
+  constructor(private userService: UserService, private router: Router) {}
 
-  constructor() { }
+  ngOnInit() {}
 
-  ngOnInit() {
+  submit() {
+    let formData = new FormData();
+    const data = {
+      username: this.username,
+      password: this.password,
+      client_id: "6739242b-df57-44fd-a8e7-0ca08f244648",
+      client_secret: "secret",
+      grant_type: "password"
+    };
+    for (let key in data) {
+      formData.append(key, data[key]);
+    }
+    this.userService.login(formData).then(res => {
+      localStorage.setItem("token", "Bearer " + res.data.access_token);
+      this.router.navigate(["/"]);
+    });
   }
-
 }
